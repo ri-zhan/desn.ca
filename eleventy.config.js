689ff53b93d17c710255210f3f60a1db5bf6482e
Ri Zhan <git@ri.works>
2025-09-14 19:23:48 -0700

    fix: 11ty config input src

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,5 +1,22 @@
 import yaml from "js-yaml";
+import { HtmlBasePlugin } from "@11ty/eleventy";
+
 
 export default function (eleventyConfig) {
 	eleventyConfig.addDataExtension("yaml", (contents) => yaml.load(contents));
+	eleventyConfig.addPlugin(HtmlBasePlugin);
+	eleventyConfig.addPassthroughCopy("src/css");
+	eleventyConfig.addPassthroughCopy("src/images");
+	eleventyConfig.addPassthroughCopy("src/js");
+
+	return {
+		dir: {
+			input: 'src',
+			output: '_site',
+		},
+		// templateFormats: ['html', 'md', 'liquid'],
+		// htmlTemplateEngine: 'liquid',
+	};
 };
+
+// module.exports = '/{{ page.filePathStem }}.html'
